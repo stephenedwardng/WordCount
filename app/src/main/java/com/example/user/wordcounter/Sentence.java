7de7941b6b5bfabd5f3e335951c0628a79cb5edd
Stephen Edward Ng <stephenedwardng@outlook.com>
2017-07-03 16:33:01 +0100

    WordCount passing

---
+++ app/src/main/java/com/example/user/wordcounter/Sentence.java
@@ -0,0 +1,28 @@
+package com.example.user.wordcounter;
+
+/**
+ * Created by user on 03/07/2017.
+ */
+
+public class Sentence {
+
+    private String sentence;
+
+    public Sentence(String sentence) {
+        this.sentence = sentence;
+    }
+
+    public int countWords() {
+        int wordCount = 1;
+
+        for(int count = 0; count < sentence.length(); count++)
+        {
+            if(sentence.charAt(count)==' ')
+                wordCount++;
+        }
+
+        return wordCount;
+    }
+
+
+}
---
+++ app/src/test/java/com/example/user/wordcounter/SentenceTest.java
@@ -0,0 +1,34 @@
+package com.example.user.wordcounter;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+/**
+ * Created by user on 03/07/2017.
+ */
+
+public class SentenceTest {
+
+    Sentence sentence;
+
+    @Before
+    public void setup() throws Exception {
+
+        sentence = new Sentence("The owls are not what they seem");
+
+    }
+
+    @Test
+    public void canCountWords() throws Exception{
+
+        assertEquals(7, sentence.countWords());
+
+    }
+
+
+
+}
